0beb8c8780524a76b67a4bb004a2447ef4a58617
Ahmed <achaouch@WEBNET.LOCAL>
2018-07-04 11:08:08 +0200

    Corrction Erreur de compilation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LivraisonComponent } from './livraison/livraison.component'
+import { LivraisonComponent } from './livraison/livraison.component';
 
 @NgModule({
   declarations: [
